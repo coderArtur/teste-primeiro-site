611ad2cd28ab343fae75c8b4f1da517f875d4f5e
coderArtur <coderartur@gmail.com>
2023-03-11 22:01:15 -0300

    att page1

--- js/page1.js
+++ js/page1.js
@@ -1,15 +1,24 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+
+
 window.addEventListener("load",()=>{
     const input = document.getElementById("upload");
-    const filewrapper =document.getElementById("filewrapper");
+    const filewrapper = document.getElementById("filewrapper");
     
     input.addEventListener("change",(e)=>{
         let fileName = e.target.files[0].name;
-        let filetype= e.target.value.split(".").pop();
+        let filetype = e.target.value.split(".").pop();
         fileshow(fileName, filetype);
     })
 
     const fileshow=(fileName, filetype)=>{
-        const showfileboxElem= document.createElement("div");
+        const showfileboxElem = document.createElement("div");
         showfileboxElem.classList.add("showfilebox");
         const leftElem = document.createElement("div");
         leftElem.classList.add("left");
@@ -17,14 +26,17 @@ window.addEventListener("load",()=>{
         fileTypeElem.classList.add("filetype");
         fileTypeElem.innerHTML=filetype;
         leftElem.append(fileTypeElem);
+        const cortarElem = document.createElement("div");
+        cortarElem.classList.add("cortar");
+        leftElem.append(cortarElem);
         const filetitleElem = document.createElement("h3");
         filetitleElem.innerHTML=fileName;
-        leftElem.append(filetitleElem);
+        cortarElem.append(filetitleElem);
         showfileboxElem.append(leftElem);
         const rightElem = document.createElement("div");
         rightElem.classList.add("right");
         showfileboxElem.append(rightElem);
-        const crossElem =document.createElement("span");
+        const crossElem = document.createElement("span");
         crossElem.innerHTML="&#215;";
         rightElem.append(crossElem);
         filewrapper.append(showfileboxElem);
