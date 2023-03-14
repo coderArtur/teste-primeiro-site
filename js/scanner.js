1ac9f8596ac82b729cd00f056e063a7e79a68b13
coderArtur <coderartur@gmail.com>
2023-03-13 23:03:32 -0300

    script page add

--- js/scanner.js
+++ js/scanner.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
+menu.onclick = ()=>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
