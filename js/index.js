0cd58db3c7699c6460df8ea3e850330dbfc4d9db
coderArtur <coderartur@gmail.com>
2023-03-23 19:45:13 -0300

    popup forgot password

--- js/index.js
+++ js/index.js
@@ -1,9 +1,22 @@
-const loginr=document.querySelector('.box');
-const Loginlink=document.querySelector('.Login-link');
-const registerlink=document.querySelector('.register-link');
+const loginr = document.querySelector('.box');
+const Loginlink = document.querySelector('.Login-link');
+const registerlink = document.querySelector('.register-link');
 registerlink.addEventListener('click',()=>{
     loginr.classList.add('active')
 });
 Loginlink.addEventListener('click',()=>{
     loginr.classList.remove('active')
+});
+
+
+const button = document.querySelector('#button');
+const popup = document.querySelector('.popup-wrapper');
+const closeButton = document.querySelector('.popup-close');
+
+button.addEventListener('click',()=>{
+    popup.style.display = 'block'
+});
+
+closeButton.addEventListener('click',()=>{
+    popup.style.display = 'none'
 });
